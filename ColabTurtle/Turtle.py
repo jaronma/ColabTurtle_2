0b1488354b497dc95d1a15e360f21417fc0ab87b
Jaron <jaronxma@gmail.com>
2020-12-11 16:01:45 -0600

    attempt 4 on arcs

--- ColabTurtle/Turtle.py
+++ ColabTurtle/Turtle.py
@@ -128,7 +128,7 @@ def _moveToNewPosition(new_pos):
 
     start_pos = turtle_pos
     if is_pen_down:
-        svg_lines_string += """<line x1="{x1}" y1="{y1}" x2="{x2}" y2="{y2}" stroke-linecap="round" style="stroke:{pen_color};stroke-width:{pen_width}"/>""".format(
+        svg_lines_string += """<line x1="{x1}" y1="{y1}" x2="{x2}" y2="{y2}" stroke-linecap="round" style="stroke:{pen_color};stroke-width:{pen_width};/>""".format(
             x1=start_pos[0], y1=start_pos[1], x2=new_pos[0], y2=new_pos[1], pen_color=pen_color, pen_width=pen_width)
 
     turtle_pos = new_pos
@@ -141,7 +141,7 @@ def _arctoNewPosition(r,new_pos):
     
     start_pos = turtle_pos
     if is_pen_down:
-        svg_lines_string += """<path d="M {x1} {y1} A {rx} {ry} 0 0 1 {x2} {y2}" stroke-linecap="round" style="stroke:{pen_color};stroke-width:{pen_width}"/>""".format(
+        svg_lines_string += """<path d="M {x1} {y1} A {rx} {ry} 0 0 1 {x2} {y2}" stroke-linecap="round" fill="transparent" style="stroke:{pen_color};stroke-width:{pen_width}"/>""".format(
             x1=start_pos[0], y1=start_pos[1],rx = r, ry = r, x2=new_pos[0], y2=new_pos[1], pen_color=pen_color, pen_width=pen_width)    
     
     turtle_pos = new_pos
