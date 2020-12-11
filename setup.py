1496ff0c63f843332fe7ce219822eceea852e074
Jaron <jaronxma@gmail.com>
2020-12-11 14:16:30 -0600

    setup.py revised

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from distutils.core import setup
 
 setup(
     name='ColabTurtle',
-    version='2.0.0',
+    version='2.0.1',
     packages=['ColabTurtle'],
-    url='https://github.com/tolgaatam/ColabTurtle',
+    url='https://github.com/jaronma/ColabTurtle_2',
     license='MIT',
-    author='Tolga Atam',
-    author_email='atam@sabanciuniv.edu',
+    author='Jaron Ma',
+    author_email='jama@u.northwestern.edu',
     description='An HTML based Turtle implementation, in order to work in Google Colab'
 )
